ac18039abe8073f71f549c320c8d42419ec142d2
Alberto Andria <alberto.andria@mercadolibre.com>
2019-10-17 13:58:51 -0300

    structure

---
+++ src/main/java/com/meli/mercadolense/MercadolenseApplication.java
@@ -0,0 +1,13 @@
+package com.meli.mercadolense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadolenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadolenseApplication.class, args);
+	}
+
+}
